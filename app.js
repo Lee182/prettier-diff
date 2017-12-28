a149fe97d8beb8dd3293c2d914d0220e249ff28b
Jonathan T L Lee <jono-lee@hotmail.co.uk>
2017-12-28 09:11:13 +0000

    added html2pug transform

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const path = require('path')
 const prettier = require('prettier')
 const uglifyjs = require('uglify-js')
+const html2pug = require('html2pug')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,10 +13,10 @@ app.use(cors())
 app.use('/', express.static(path.join(__dirname, '/dist')))
 
 app.post('/transform-code', (req, res) => {
-  let {code, options, name} = req.body
+  let { code, options, name } = req.body
   const transforms = {
     prettier: prettier.format,
-    uglifyjs (code, options) {
+    uglifyjs(code, options) {
       const result = uglifyjs.minify(code)
       return result.code || code
     }
@@ -24,11 +25,18 @@ app.post('/transform-code', (req, res) => {
     return res.send(401)
   }
   // validate options
-  code = code.map((strCode) => {
-    return transforms[name](strCode, {semi: false})
+  code = code.map(strCode => {
+    return transforms[name](strCode, { semi: false })
   })
-  res.json({code, options, date: new Date()})
+  res.json({ code, options, date: new Date() })
 })
+
+app.post('/html2pug', (req, res) => {
+  const { code } = req.body
+  const sPug = html2pug(code)
+  res.json({ sPug })
+})
+
 app.listen(9700, () => {
   console.log(9700)
 })
